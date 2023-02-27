8595a202bdb45e6252ee8c7f9774bfc0587af553
Michael Cardell Widerkrantz <mc@tillitis.se>
2023-02-27 17:50:14 +0100

    fw: Hide HTIF functions better when using NOCONSOLE
    We define macros for them that expand to nothing or to a constant to
    avoid any extra function calls to dummy functions when running on real
    hardware with no console.

--- hw/application_fpga/fw/tk1/lib.h
+++ hw/application_fpga/fw/tk1/lib.h
@@ -8,12 +8,22 @@
 
 #include "types.h"
 
+#ifdef NOCONSOLE
+#define htif_putc(ch)
+#define htif_lf()
+#define htif_puthex(c)
+#define htif_putinthex(n)
+#define htif_puts(s) ((int)0)
+#define htif_hexdump(buf, len)
+#else
 void htif_putc(int ch);
 void htif_lf();
 void htif_puthex(uint8_t c);
 void htif_putinthex(const uint32_t n);
 int htif_puts(const char *s);
 void htif_hexdump(uint8_t *buf, int len);
+#endif
+
 void *memset(void *dest, int c, unsigned n);
 void memcpy_s(void *dest, size_t destsize, const void *src, size_t n);
 void wordcpy_s(void *dest, size_t destsize, const void *src, size_t n);
