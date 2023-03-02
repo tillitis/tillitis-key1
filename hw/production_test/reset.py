d7aecb9267e846f5e8d25accaa5e258759f6df51
Matt Mets <matt@blinkinlabs.com>
2023-03-02 23:52:52 +0100

    Lint more python scripts

--- hw/production_test/reset.py
+++ hw/production_test/reset.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
-import hid_test
+import hid_test  # type: ignore
 import time
 
 
-def reset_tk1():
-    """Manipulate the GPIO lines on the MTA1-USB-CH552 Programmer to issue a
-    hardware reset to the TK1. The result is that TK1 again will be in firmware
-    mode, so a new app can be loaded."""
+def reset_tk1() -> None:
+    """ Reset a TK1 contained in a TP1 programmer
+
+    Manipulate the GPIO lines on the TP1 to issue a hardware reset
+    to the TK1. The result is that TK1 again will be in firmware
+    mode, so a new app can be loaded.
+    """
     d = hid_test.ice40_flasher()
     d.gpio_set_direction(14, True)
     d.gpio_put(14, False)
     d.gpio_set_direction(14, False)
     d.close()
+
 
 reset_tk1()
