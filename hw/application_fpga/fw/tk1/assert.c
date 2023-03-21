f622937918469f574af660eefc1094297a028e56
Michael Cardell Widerkrantz <mc@tillitis.se>
2023-03-21 13:56:07 +0100

    fw: Don't use reserved or reserved-looking names

--- hw/application_fpga/fw/tk1/assert.c
+++ hw/application_fpga/fw/tk1/assert.c
@@ -7,17 +7,17 @@
 #include "led.h"
 #include "lib.h"
 
-void __assert_fail(const char *__assertion, const char *__file,
-		   unsigned int __line, const char *__function)
+void assert_fail(const char *assertion, const char *file,
+		   unsigned int line, const char *function)
 {
 	htif_puts("assert: ");
-	htif_puts(__assertion);
+	htif_puts(assertion);
 	htif_puts(" ");
-	htif_puts(__file);
+	htif_puts(file);
 	htif_puts(":");
-	htif_putinthex(__line);
+	htif_putinthex(line);
 	htif_puts(" ");
-	htif_puts(__function);
+	htif_puts(function);
 	htif_lf();
 
 	forever_redflash();
