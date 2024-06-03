cadf8e9849d56b255524ed96387c0707c010ad14
Joachim Strömbergson <joachim@assured.se>
2024-06-03 15:07:01 +0200

    FPGA: Add sim model of udi_rom
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

---
+++ hw/application_fpga/core/tk1/tb/udi_rom_sim.v
@@ -0,0 +1,36 @@
+//======================================================================
+//
+// udi_rom_sim.v
+// ---------
+// Simulation version of the UDI ROM.
+//
+//
+// Author: Joachim Strömbergson.
+// Copyright (C) 2023 - Tillitis AB
+// SPDX-License-Identifier: GPL-2.0-only
+//
+//======================================================================
+
+module udi_rom (
+		input wire [0:0] addr,
+		output wire [31:0] data
+		);
+
+  reg [31 : 0] tmp_data;
+  assign data = tmp_data;
+
+  always @*
+    begin : addr_mux
+      if (addr) begin
+	tmp_data = 32'h04050607;
+      end
+      else begin
+	tmp_data = 32'h00010203;
+      end
+    end
+
+endmodule // udi_rom
+
+//======================================================================
+// EOF udi_rom_sim.v
+//======================================================================
