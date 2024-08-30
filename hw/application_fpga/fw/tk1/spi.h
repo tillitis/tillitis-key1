a5c2d05cb130b4a8fce7208baf5463296c5cd4d7
Daniel Jobson <jobson@tillitis.se>
2024-08-30 14:10:40 +0200

    Import spi.[ch] and flash.[ch]

---
+++ hw/application_fpga/fw/tk1/spi.h
@@ -0,0 +1,15 @@
+// Copyright (C) 2024 - Tillitis AB
+// SPDX-License-Identifier: GPL-2.0-only
+
+#ifndef TKEY_SPI_H
+#define TKEY_SPI_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+int spi_ready(void);
+int spi_write(uint8_t *cmd, size_t size_cmd, uint8_t *data, size_t size_data);
+int spi_transfer(uint8_t *tx_buf, size_t tx_size, uint8_t *rx_buf,
+		 size_t rx_size);
+
+#endif
