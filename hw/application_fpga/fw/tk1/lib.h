5188584fcfcbdabb98bc5ab094f5418b6c1273f4
Daniel Jobson <jobson@tillitis.se>
2024-09-04 10:02:06 +0200

    fw: Break out htif functions for qemu to separate files

--- hw/application_fpga/fw/tk1/lib.h
+++ hw/application_fpga/fw/tk1/lib.h
@@ -9,25 +9,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#ifdef QEMU_CONSOLE
-void htif_putc(char ch);
-void htif_lf(void);
-void htif_puthex(uint8_t c);
-void htif_putinthex(const uint32_t n);
-void htif_puts(const char *s);
-void htif_hexdump(void *buf, int len);
-#else
-#define htif_putc(ch)
-#define htif_lf(void)
-#define htif_puthex(c)
-#define htif_putinthex(n)
-#define htif_puts(s)
-#define htif_hexdump(buf, len)
-#endif /* QEMU_CONSOLE */
-
 void *memset(void *dest, int c, unsigned n);
 void memcpy_s(void *dest, size_t destsize, const void *src, size_t n);
 void wordcpy_s(void *dest, size_t destsize, const void *src, size_t n);
 int memeq(void *dest, const void *src, size_t n);
 void secure_wipe(void *v, size_t n);
+
 #endif
