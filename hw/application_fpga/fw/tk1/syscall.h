3ec0e5bd3ee0c75ce7ce7a2d2dd452759c593a67
Daniel Jobson <jobson@tillitis.se>
2024-09-17 14:54:19 +0200

    Wip syscall function.
    PoC of how a syscall could look like.

---
+++ hw/application_fpga/fw/tk1/syscall.h
@@ -0,0 +1,35 @@
+// Copyright (C) 2024 - Tillitis AB
+// SPDX-License-Identifier: GPL-2.0-only
+
+#ifndef SYSCALL_H
+#define SYSCALL_H
+
+#include "partition_table.h"
+
+#include <stdint.h>
+#include <stddef.h>
+
+
+typedef struct {
+	uint8_t syscall_no;
+	uint32_t offset;
+	uint8_t *data;
+	size_t size;
+	uint32_t *ctx;
+} syscall_t;
+
+enum syscall_cmd {
+	BLAKE2S = 0,
+	ALLOC_AREA,
+	DEALLOC_AREA,
+	WRITE_DATA,
+	READ_DATA,
+	PRELOAD_STORE,
+	PRELOAD_DELETE,
+	MGMT_APP_REGISTER,
+	MGMT_APP_UNREGISTER,
+};
+
+int syscall(syscall_t *ctx);
+
+#endif
