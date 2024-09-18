26045bfcd1026ce2d1984e40023bacef362f5dc5
Daniel Jobson <jobson@tillitis.se>
2024-09-18 11:11:13 +0200

    fw: use bool as return type for memeq

--- hw/application_fpga/fw/tk1/lib.h
+++ hw/application_fpga/fw/tk1/lib.h
@@ -8,11 +8,12 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 void *memset(void *dest, int c, unsigned n);
 void memcpy_s(void *dest, size_t destsize, const void *src, size_t n);
 void wordcpy_s(void *dest, size_t destsize, const void *src, size_t n);
-int memeq(void *dest, const void *src, size_t n);
+bool memeq(void *dest, const void *src, size_t n);
 void secure_wipe(void *v, size_t n);
 
 #endif
