34aeefe35094442f8832842d704b09226c8a30f8
Daniel Jobson <jobson@tillitis.se>
2024-11-07 13:10:10 +0100

    Add API for syscall
    Add a register to store an address to a syscall function defined in
    firmware. Set the reset value to an illegal address, to make sure a call
    to an unset address will halt the CPU.
    
    Co-authored-by: Mikael Ågren <mikael@tillitis.se>

--- hw/application_fpga/core/tk1/rtl/tk1.v
+++ hw/application_fpga/core/tk1/rtl/tk1.v
@@ -78,6 +78,7 @@ module tk1 (
   localparam ADDR_APP_SIZE = 8'h0d;
 
   localparam ADDR_BLAKE2S = 8'h10;
+  localparam ADDR_SYSCALL = 8'h12;
 
   localparam ADDR_CDI_FIRST = 8'h20;
   localparam ADDR_CDI_LAST = 8'h27;
@@ -106,6 +107,10 @@ module tk1 (
   localparam FW_RAM_LAST = 32'hd00007ff;
 
 
+  // ILLEGAL_ADDR is outside of the physical memory, and will trap in
+  // the security monitor if accessed.
+  localparam ILLEGAL_ADDR = 32'h4f000000;
+
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
   //----------------------------------------------------------------
@@ -133,6 +138,9 @@ module tk1 (
 
   reg  [31 : 0] blake2s_addr_reg;
   reg           blake2s_addr_we;
+
+  reg  [31 : 0] syscall_addr_reg;
+  reg           syscall_addr_we;
 
   reg  [23 : 0] cpu_trap_ctr_reg;
   reg  [23 : 0] cpu_trap_ctr_new;
@@ -257,6 +265,7 @@ module tk1 (
       app_start_reg     <= 32'h0;
       app_size_reg      <= 32'h0;
       blake2s_addr_reg  <= 32'h0;
+      syscall_addr_reg  <= ILLEGAL_ADDR;
       cdi_mem[0]        <= 32'h0;
       cdi_mem[1]        <= 32'h0;
       cdi_mem[2]        <= 32'h0;
@@ -313,6 +322,10 @@ module tk1 (
 
       if (blake2s_addr_we) begin
         blake2s_addr_reg <= write_data;
+      end
+
+      if (syscall_addr_we) begin
+        syscall_addr_reg <= write_data;
       end
 
       if (cdi_mem_we) begin
@@ -421,6 +434,7 @@ module tk1 (
     app_start_we     = 1'h0;
     app_size_we      = 1'h0;
     blake2s_addr_we  = 1'h0;
+    syscall_addr_we  = 1'h0;
     cdi_mem_we       = 1'h0;
     ram_addr_rand_we = 1'h0;
     ram_data_rand_we = 1'h0;
@@ -473,6 +487,12 @@ module tk1 (
         if (address == ADDR_BLAKE2S) begin
           if (!system_mode_reg) begin
             blake2s_addr_we = 1'h1;
+          end
+        end
+
+        if (address == ADDR_SYSCALL) begin
+          if (!system_mode_reg) begin
+            syscall_addr_we = 1'h1;
           end
         end
 
@@ -558,6 +578,10 @@ module tk1 (
 
         if (address == ADDR_BLAKE2S) begin
           tmp_read_data = blake2s_addr_reg;
+        end
+
+        if (address == ADDR_SYSCALL) begin
+          tmp_read_data = syscall_addr_reg;
         end
 
         if ((address >= ADDR_CDI_FIRST) && (address <= ADDR_CDI_LAST)) begin
