b45da9bf0cf301ad98ea78265d77f6d46d6230a3
Daniel Jobson <jobson@tillitis.se>
2024-11-15 11:19:40 +0100

    PoC: Make sensitive assets only readable/writable before system_mode is set
    After the first time system_mode is set to one, the assets will no
    longer be read- or writeable, even if system_mode is set to zero at a
    later syscall. This is to make sure syscalls does not have the same
    privilege as the firmware has at first boot.
    
    We need to monitor when system_mode is set to one, otherwise we might
    accedentially lock the assets before actually leaving firmware, for
    example if firmware would use a function set in any of the registers
    used in system_mode_ctrl.
    
    Co-authored-by: Mikael Ågren <mikael@tillitis.se>

--- hw/application_fpga/tb/application_fpga_sim.v
+++ hw/application_fpga/tb/application_fpga_sim.v
@@ -163,7 +163,7 @@ module application_fpga_sim (
   reg  [31 : 0] tk1_write_data;
   wire [31 : 0] tk1_read_data;
   wire          tk1_ready;
-  wire          system_mode;
+  wire          rw_locked;
   wire          force_trap;
   wire [14 : 0] ram_addr_rand;
   wire [31 : 0] ram_data_rand;
@@ -302,7 +302,7 @@ module application_fpga_sim (
       .clk(clk),
       .reset_n(reset_n),
 
-      .system_mode(system_mode),
+      .en(rw_locked),
 
       .cs(uds_cs),
       .address(uds_address),
@@ -347,7 +347,7 @@ module application_fpga_sim (
       .clk(clk),
       .reset_n(reset_n),
 
-      .system_mode(system_mode),
+      .rw_locked  (rw_locked),
 
       .cpu_addr  (cpu_addr),
       .cpu_instr (cpu_instr),
