2cb5f2eca67bbec998b5b073c7beda9d2882ba88
Michael Cardell Widerkrantz <mc@tillitis.se>
2025-03-13 14:18:38 +0100

    Add start of pre-loaded app from flash
    Based on earlier code by Daniel Jobson <jobson@tillitis.se> now
    integrated into the new world order.
    
    Co-authored-by: Mikael Ågren <mikael@tillitis.se>
    Co-authored-by: Daniel Jobson <jobson@tillitis.se>

---
+++ hw/application_fpga/fw/tk1/preload_app.h
@@ -0,0 +1,20 @@
+// Copyright (C) 2024 - Tillitis AB
+// SPDX-License-Identifier: GPL-2.0-only
+
+#ifndef PRELOAD_APP_H
+#define PRELOAD_APP_H
+
+#include "partition_table.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+bool preload_check_valid_app(struct partition_table *part_table);
+int preload_load(struct partition_table *part_table);
+int preload_store(struct partition_table *part_table, uint32_t offset,
+		  uint8_t *data, size_t size);
+int preload_store_finalize(struct partition_table *part_table, bool use_uss,
+			   uint8_t *uss, size_t app_size);
+int preload_delete(struct partition_table *part_table);
+
+#endif
