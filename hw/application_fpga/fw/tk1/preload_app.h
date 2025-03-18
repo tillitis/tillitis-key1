9018ae5b332bb680f7fed4c1e40986caf34fcb6c
Mikael Ågren <mikael@tillitis.se>
2025-03-18 16:25:49 +0100

    Store app digest and signature for each app slot

--- hw/application_fpga/fw/tk1/preload_app.h
+++ hw/application_fpga/fw/tk1/preload_app.h
@@ -14,7 +14,9 @@ bool preload_check_valid_app(struct partition_table *part_table,
 int preload_load(struct partition_table *part_table, uint8_t from_slot);
 int preload_store(struct partition_table *part_table, uint32_t offset,
 		  uint8_t *data, size_t size, uint8_t to_slot);
-int preload_store_finalize(struct partition_table *part_table, size_t app_size, uint8_t to_slot);
+int preload_store_finalize(struct partition_table *part_table, size_t app_size,
+			   uint8_t app_digest[32], uint8_t app_signature[64],
+			   uint8_t to_slot);
 int preload_delete(struct partition_table *part_table, uint8_t slot);
 
 #endif
