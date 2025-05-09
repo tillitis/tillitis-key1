9abdcef34c281886a8f1fda0aa7d80925b9aa6cb
Mikael Ågren <mikael@tillitis.se>
2025-05-09 17:14:38 +0200

    ch552: Make CTS pins configurable

--- hw/usb_interface/ch552_fw/src/gpio.c
+++ hw/usb_interface/ch552_fw/src/gpio.c
@@ -5,18 +5,6 @@
 
 #include "ch554.h"
 
-// Set pin p1.4 and p1.5 to GPIO output mode.
-void gpio_init()
-{
-    // p1.4
-    P1_MOD_OC &= ~0x10;
-    P1_DIR_PU |= 0x10;
-
-    // p1.5
-    P1_MOD_OC &= ~0x20;
-    P1_DIR_PU |= 0x20;
-}
-
 void gpio_set(uint8_t pin)
 {
     switch (pin) {
@@ -64,48 +52,26 @@ uint8_t gpio_get(uint8_t pin)
     return ret;
 }
 
-// Set pin p1.4 to GPIO input mode. (FPGA_CTS)
-void gpio_init_p1_4_in()
+void gpio_dir_in(uint8_t pin)
 {
-    // p1.4
-    P1_MOD_OC &= ~0x10; // Output Mode: 0 = Push-pull output, 1 = Open-drain output
-    P1_DIR_PU &= ~0x10; // Port Direction Control and Pull-up Enable Register:
-                        //    Push-pull output mode:
-                        //        0 = Input.
-                        //        1 = Output
-                        //    Open-drain output mode:
-                        //        0 = Pull-up resistor disabled
-                        //        1 = Pull-up resistor enabled
+    P1_MOD_OC &= ~pin; // Output Mode: 0 = Push-pull output, 1 = Open-drain output
+    P1_DIR_PU &= ~pin; // Port Direction Control and Pull-up Enable Register:
+                       //    Push-pull output mode:
+                       //        0 = Input.
+                       //        1 = Output
+                       //    Open-drain output mode:
+                       //        0 = Pull-up resistor disabled
+                       //        1 = Pull-up resistor enabled
 }
 
-// Read status of pin 1.4
-uint8_t gpio_p1_4_get(void)
+void gpio_dir_out(uint8_t pin)
 {
-    return (P1 & 0x10); // p1.4
-}
-
-// Set pin p1.5 to GPIO output mode. (CH552_CTS)
-void gpio_init_p1_5_out()
-{
-    // p1.5
-    P1_MOD_OC &= ~0x20; // Output Mode: 0 = Push-pull output, 1 = Open-drain output
-    P1_DIR_PU |=  0x20; // Port Direction Control and Pull-up Enable Register:
-                        //    Push-pull output mode:
-                        //        0 = Input.
-                        //        1 = Output
-                        //    Open-drain output mode:
-                        //        0 = Pull-up resistor disabled
-                        //        1 = Pull-up resistor enabled
-}
-
-// Set pin 1.5 high
-void gpio_p1_5_set(void)
-{
-    P1 |= 0x20;
-}
-
-// Set pin 1.5 low
-void gpio_p1_5_unset(void)
-{
-    P1 &= ~0x20;
+    P1_MOD_OC &= ~pin; // Output Mode: 0 = Push-pull output, 1 = Open-drain output
+    P1_DIR_PU |=  pin; // Port Direction Control and Pull-up Enable Register:
+                       //    Push-pull output mode:
+                       //        0 = Input.
+                       //        1 = Output
+                       //    Open-drain output mode:
+                       //        0 = Pull-up resistor disabled
+                       //        1 = Pull-up resistor enabled
 }
