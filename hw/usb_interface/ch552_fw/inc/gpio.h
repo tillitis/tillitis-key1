bb071014c31ec524a05fc15cd8ef6af4e0af3e6f
Mikael Ågren <mikael@tillitis.se>
2025-05-09 17:29:52 +0200

    fpga/ch552: Swap fpga_cts and ch552_cts pins
    The FPGA uwg30 package cannot use B3 as an input when an instance of
    SB_PLL40_CORE is placed. We swap fpga_cts and ch552_cts to make B3 (from
    here on fpga_cts) an output.
    
    For more info check out:
    FPGA-TN-02052-1-4-iCE40-sysCLOCK-PLL-Design-User-Guide.pdf chapter "5.1
    PLL Placement Rules"

--- hw/usb_interface/ch552_fw/inc/gpio.h
+++ hw/usb_interface/ch552_fw/inc/gpio.h
@@ -9,8 +9,8 @@
 #define PIN_P1_4 0x10
 #define PIN_P1_5 0x20
 
-#define PIN_FPGA_CTS PIN_P1_4
-#define PIN_CH552_CTS PIN_P1_5
+#define PIN_FPGA_CTS PIN_P1_5
+#define PIN_CH552_CTS PIN_P1_4
 
 void gpio_set(uint8_t pin);
 void gpio_unset(uint8_t pin);
