edf76d52b9d95eebb200264d31771765ab1d7969
Mikael Ågren <mikael@tillitis.se>
2025-05-09 14:22:14 +0200

    fpga: Experimental fpga build for uwg30 package
    nextpnr-ice40 fails with:
    
    ```
    Info: Placing PLLs..
    ERROR: PLL 'reset_gen_inst.pll_inst' couldn't be placed anywhere, no
    suitable BEL found.
        PLL bel 'X12/Y31/pll_3' cannot be used as it conflicts with input
    'interface_ch552_cts$sb_io' on pin 'B3'.
    ```

--- hw/application_fpga/rtl/application_fpga.v
+++ hw/application_fpga/rtl/application_fpga.v
@@ -30,10 +30,10 @@ module application_fpga (
 
     input wire touch_event,
 
-    input  wire app_gpio1,
-    input  wire app_gpio2,
-    output wire app_gpio3,
-    output wire app_gpio4,
+    // input  wire app_gpio1,
+    // input  wire app_gpio2,
+    // output wire app_gpio3,
+    // output wire app_gpio4,
 
     output wire led_r,
     output wire led_g,
@@ -364,10 +364,10 @@ module application_fpga (
       .led_g(led_g),
       .led_b(led_b),
 
-      .gpio1(app_gpio1),
-      .gpio2(app_gpio2),
-      .gpio3(app_gpio3),
-      .gpio4(app_gpio4),
+      .gpio1(),
+      .gpio2(),
+      .gpio3(),
+      .gpio4(),
 
       .syscall(irq31_eoi),
 
