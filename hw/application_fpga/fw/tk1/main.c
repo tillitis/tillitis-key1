20bcbb8b3a4bfbbbb2d75583bd8188ebdc66929d
Mikael Ågren <mikael@tillitis.se>
2026-02-05 14:09:20 +0100

    fw: Remove ctx.flash_slot in favor of ctx.app_source to avoid duplicated state

--- hw/application_fpga/fw/tk1/main.c
+++ hw/application_fpga/fw/tk1/main.c
@@ -58,10 +58,9 @@ struct context {
 	uint8_t *loadaddr;  // Where we are currently loading a TKey program
 	bool use_uss;	    // Use USS?
 	uint8_t uss[32];    // User Supplied Secret, if any
-	uint8_t flash_slot; // App is loaded from flash slot number
-	/*@null@*/ volatile uint8_t
-	    *ver_digest;	 // Verify loaded app against this digest
 	enum app_source app_src; // Where app was loaded from
+	/*@null@*/ volatile uint8_t
+	    *ver_digest; // Verify loaded app against this digest
 };
 
 static void print_hw_version(void);
@@ -82,7 +81,7 @@ static uint32_t xorwow(uint32_t state, uint32_t acc);
 static void scramble_ram(void);
 static int compute_app_digest(uint8_t *digest);
 static int load_flash_app(struct partition_table *part_table,
-			  uint8_t digest[32], uint8_t slot);
+			  uint8_t digest[32], enum app_source src);
 static enum state start_where(struct context *ctx);
 
 static void print_hw_version(void)
@@ -391,8 +390,23 @@ static void jump_to_app(void)
 }
 
 static int load_flash_app(struct partition_table *part_table,
-			  uint8_t digest[32], uint8_t slot)
+			  uint8_t digest[32], enum app_source src)
 {
+	uint8_t slot = 0;
+
+	switch (src) {
+	case APP_SRC_FLASH0:
+		slot = 0;
+		break;
+
+	case APP_SRC_FLASH1:
+		slot = 1;
+		break;
+
+	default:
+		return -1;
+	}
+
 	if (slot >= N_PRELOADED_APP) {
 		return -1;
 	}
@@ -479,28 +493,24 @@ static enum state start_where(struct context *ctx)
 	case START_DEFAULT:
 		// fallthrough
 	case START_FLASH0:
-		ctx->flash_slot = 0;
 		ctx->app_src = APP_SRC_FLASH0;
 		ctx->ver_digest = mgmt_app_allowed_digest();
 
 		return FW_STATE_LOAD_FLASH_MGMT;
 
 	case START_FLASH1:
-		ctx->flash_slot = 1;
 		ctx->app_src = APP_SRC_FLASH1;
 		ctx->ver_digest = NULL;
 
 		return FW_STATE_LOAD_FLASH;
 
 	case START_FLASH0_VER:
-		ctx->flash_slot = 0;
 		ctx->app_src = APP_SRC_FLASH0;
 		ctx->ver_digest = resetinfo->app_digest;
 
 		return FW_STATE_LOAD_FLASH;
 
 	case START_FLASH1_VER:
-		ctx->flash_slot = 1;
 		ctx->app_src = APP_SRC_FLASH1;
 		ctx->ver_digest = resetinfo->app_digest;
 
@@ -589,7 +599,7 @@ int main(void)
 
 		case FW_STATE_LOAD_FLASH:
 			if (load_flash_app(&part_table_storage.table,
-					   ctx.digest, ctx.flash_slot) < 0) {
+					   ctx.digest, ctx.app_src) < 0) {
 				debug_puts("Couldn't load app from flash\n");
 				state = FW_STATE_FAIL;
 				break;
@@ -600,7 +610,7 @@ int main(void)
 
 		case FW_STATE_LOAD_FLASH_MGMT:
 			if (load_flash_app(&part_table_storage.table,
-					   ctx.digest, ctx.flash_slot) < 0) {
+					   ctx.digest, ctx.app_src) < 0) {
 				debug_puts("Couldn't load app from flash\n");
 				state = FW_STATE_FAIL;
 				break;
